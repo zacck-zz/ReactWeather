f6ceeebdfe3b659e0c3e1d5e39627e0a82155bc6
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-07-24 18:12:55 +0200

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
-    <h3>Examples Component!</h3>
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to Exmples Page</p>
+    </div>
   );
 }
 
