7766b40774c83184e7a974c3f0f092737285d36b
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-07-26 10:36:58 +0200

    Style Nav Bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,15 +3,44 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 
-var Nav = (props) => {
-  return(
-    <div>
-      {/*Add link to base Main page to specifies path according to routes in app.jsx*/}
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('not yet wired up');
+  },
+  render: function(){
+    return(
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            ZEE WEATHER
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+          </li>
+          <li>
+            <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+          </li>
+          <li>
+            <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <form onSubmit={this.onSearch}>
+        <ul className="menu">
+          <li>
+            <input type="search" placeholde="Search Weather"/>
+          </li>
+          <li>
+            <input type="submit" className="button" value="Get Weather"/>
+          </li>
+        </ul>
+        </form>
+      </div>
     </div>
-  );
-}
-
+    );
+  }
+});
 module.exports = Nav;
