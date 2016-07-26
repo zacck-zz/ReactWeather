cb71d6ea8c699d96e0c572f02fc70a5011d79193
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-07-26 11:15:44 +0200

    Style About page and link to resouces

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
 
-
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to the about page</p>
+      <p>This is just a small sample application to get the hang of react</p>
+      <p>Tools Used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the javascript framework used
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - This is the api used to source the weather information
+        </li>
+      </ul>
     </div>
   );
 }
